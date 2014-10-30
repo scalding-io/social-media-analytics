37ae8a4a4af932428c99bd7a08b5e244fa8c5d64
galarragas <galarragas@gmail.com>
2014-10-30 23:11:20 +0000

    Added example runner

--- approximated-algorithms/src/main/scala/io/scalding/examples/analytics/approx/BloomFilterExampleRunner.scala
+++ approximated-algorithms/src/main/scala/io/scalding/examples/analytics/approx/BloomFilterExampleRunner.scala
@@ -6,11 +6,17 @@ import org.apache.hadoop.conf.Configuration
 
 object BloomFilterExampleRunner extends App {
 
+  val historical = "./datasets/SFPD_Incidents_Previous_Three_Months.csv"
+  val daily = "./datasets/SFPD_Incidents_Previous_Day.csv"
+  val output = "bloomFilterSPFDAnalysis.csv"
+
+  println( s"Analysing daily incident file '$daily' matching with historical incidents at '$historical' and writing the output at '$output'" )
+
   val jobArgs =
     classOf[BloomFilterExample].getName :: "--local" ::
-      "--historical" :: "./datasets/SFPD_Incidents_Previous_Three_Months.csv" ::
-      "--daily" :: "./datasets/SFPD_Incidents_Previous_Day.csv" ::
-      "--output" :: "bloomFilterSPFDAnalysis.csv" ::
+      "--historical" ::  historical ::
+      "--daily" :: daily ::
+      "--output" :: output ::
       args.toList
 
   hadoop.util.ToolRunner.run(new Configuration, new Tool, jobArgs.toArray );
