fd0a608185d7664a17261d3d348067982b6d19e7
Antonios Chalkiopoulos <Antwnis@gmail.com>
2014-12-06 17:13:01 +0000

    Polishing CSM stackexhange example

--- src/main/scala/io/scalding/approximations/CMSketch/CMSstackexchangeFields.scala
+++ src/main/scala/io/scalding/approximations/CMSketch/CMSstackexchangeFields.scala
@@ -18,71 +18,18 @@ class CMSstackexchangeFields(args: Args) extends Job(args) {
   // The input schema of our data-set
   val schema = List('ID, 'PostTypeID, 'ParentID, 'OwnerUserID, 'CreationDate, 'ViewCount, 'FavoriteCount, 'Tags, 'Keywords)
 
-  // Take in as arguments the location of the input/output files
-  val inputFiles = args("input")
-  val outputFile = args("output")
-
   // Construct a Count-Min Sketch monoid and initialize an empty sketch
   val eps: Double = 0.01
   val delta: Double = 0.02
   val seed: Int = (Math.random()*100).toInt
 
   implicit val cmsketchMonoid = new CountMinSketchMonoid(eps, delta, seed)
-  val sketch:Option[CMS] = None
 
-  val stackExchangePosts = Tsv(inputFiles,schema).read
-    .project('OwnerUserID)
-    .insert('sketch, sketch)
-    // group all to generate a single stream of tuples
+  val stackExchangePosts = Tsv(args("input"),schema).read
+    .mapTo('OwnerUserID-> 'cms) { x:Long => cmsketchMonoid.create(x) }
     .groupAll { group =>
-      // fold left the stream of tuples passing in as initial paramters
-      // to the function the `monoid` and an empty `sketch`
-      group.foldLeft(('OwnerUserID, 'sketch)->('OwnerUserID, 'sketch)) ( (cmsketchMonoid, sketch) ) { cmSketch }
+      group.reduce('cms -> 'cms) { (left:CMS,right:CMS) => left ++ right }
     }
-    .discard('OwnerUserID)
-    .map('sketch -> 'sketch) { s: Option[CMS] =>
-      s match {
-        case Some(sketch) => {
-          println(" + Total count in the CM sketch : " + sketch.totalCount)
-          println(" + Heavy Hitters : " + sketch.heavyHitters.size)
-          sketch.heavyHitters.foreach( userid => { println("  - User ID : " + userid + " with estimated cardinality : " + sketch.frequency(userid).estimate) } )
-        }
-        case _ => println("No information to display")
-      }
-      s
-    }
-    .write(Tsv(outputFile))
+    .write(Tsv(args("output")))
 
-  /*
-   * The foldLeft function that accumulates the sketch information over a stream of user IDs
-   */
-  def cmSketch(accumulator:(CountMinSketchMonoid,Option[CMS]), curr:(Long, Option[CMS])) : (CountMinSketchMonoid,Option[CMS]) = {
-    // Get the CMS monoid and item from the accumulator and the user id as the first element of the current line
-    val (cmsketchMonoid,existingSketch) = accumulator
-    val userid = curr._1
-    // Create a sketch out of a single item
-    val additionalSketch = cmsketchMonoid.create(userid)
-    // Augment the current sketch by ++ with the existing sketch
-    val newSketch = if( existingSketch.isDefined )  Some(existingSketch.get ++ additionalSketch) else Some(additionalSketch)
-    // Return the monoid and the sketch
-    (cmsketchMonoid, newSketch)
-  }
-  
 }
-
-//  val n = 100
-//  val stream:List[Long] = List.tabulate[List[Long]](n)( k => {
-//    val freq = n - k
-//    List.fill[Long](freq)( k )
-//  }).flatten
-//
-//  val result = stream.foldLeft(monoid, cmsitem)( (a,b) => {
-//    val (mymonoid, myitem) = a
-//    val number = b
-//    val newitem = updateCMS( mymonoid, myitem, number )
-//    val retval = (mymonoid, newitem)
-//    retval
-//  })._2.get
-
-// Thanks to https://github.com/krishnanraman/bigdata/blob/f41ec92e9731358c1c2ede659eb2114ee5fd07fe/CMSTest.scala
-
