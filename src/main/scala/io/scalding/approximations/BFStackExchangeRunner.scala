33566b758a7b76d7892cd1b4ec13267f33008e1e
Antonios Chalkiopoulos <Antwnis@gmail.com>
2014-12-13 16:25:56 +0000

    adding documentation

--- src/main/scala/io/scalding/approximations/BFStackExchangeRunner.scala
+++ src/main/scala/io/scalding/approximations/BFStackExchangeRunner.scala
@@ -6,14 +6,18 @@ import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.util.ToolRunner
 
 /**
- * This is effictively equivelant to
- *  $ java -jar
+ * This is effectively equivalent to
+ *
+ *  $ java -jar build/libs/scalding-approximations.jar io.scalding.approximations.BloomFilter.BFStackExchangeUsersPostExtractor \
+         --local --minAge 18 --maxAge 21 --posts datasets/stackexchange/posts.tsv \
+         --users datasets/stackexchange/users.tsv \
+         --output results/BF-StackExchangeUsersPost.tsv
  */
 object BFStackExchangeRunner extends App {
 
   ToolRunner.run(new Configuration, new Tool, (classOf[BFStackExchangeUsersPostExtractor].getName ::
     "--local" :: "--minAge" :: "18" :: "--maxAge" :: "21" ::
-    "--posts" :: "datasets/stackexchange/posts.tsv" :: "--users" :: "datasets/stackexchange/Users.xml" ::
+    "--posts" :: "datasets/stackexchange/posts.tsv" :: "--users" :: "datasets/stackexchange/users.tsv" ::
     "--output":: "results/BF-StackExchangeUsersPost.tsv" :: args.toList).toArray)
 
 }
