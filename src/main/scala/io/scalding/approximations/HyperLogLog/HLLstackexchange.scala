a7b50ca532c7b14e77e3a9e21c354791715e87a2
Antonios Chalkiopoulos <Antwnis@gmail.com>
2015-04-07 09:51:32 +0100

    HyperLogLogAggregator on stackexchange dataset

--- src/main/scala/io/scalding/approximations/HyperLogLog/HLLstackexchange.scala
+++ src/main/scala/io/scalding/approximations/HyperLogLog/HLLstackexchange.scala
@@ -1,43 +1,36 @@
 package io.scalding.approximations.HyperLogLog
 
-import cascading.pipe.Pipe
-import com.twitter.algebird.DenseHLL
+import com.twitter.algebird._
 import com.twitter.scalding._
 
 /**
- * Counting the number of unique users in the stack exchange data-set
+ * Counting the number of unique users in the stack exchange data-set using the Typed API
+ *
  * @author Antonios.Chalkiopoulos - http://scalding.io
  */
+object StackExchange {
+  type StackExchangeType = (Long, Long, Long,Long, String, Long, Long, String,String)
+  def fromTuple(t: StackExchangeType) = StackExchange(t._1, t._2, t._3, t._4, t._5,t._6,t._7,t._8,t._9)
+}
+case class StackExchange(ID: Long, PostTypeID: Long, ParentID: Long, OwnerUserID: Long, CreationDate: String,
+                ViewCount: Long, FavoriteCount: Long, Tags: String, Keywords: String)
+
 class HLLstackexchange(args: Args) extends Job(args) {
 
-  // The input schema of our data-set
-  val schema = List('ID, 'PostTypeID, 'ParentID, 'OwnerUserID, 'CreationDate, 'ViewCount, 'FavoriteCount, 'Tags, 'Keywords)
+  val input  = args.getOrElse("input" ,"datasets/stackexchange/posts.tsv")
+  val output = args.getOrElse("output","results/HLL-stackexchange")
 
-  // Take in as arguments the location of the input/output files
-  val inputFiles = args("input")
-  val outputFile = args("output")
+  // HLL Error is about 1.04/sqrt(2^{bits}), so you want something like 12 bits for ~2% error
+  // which means each HLLInstance is about 2^{12} = 4kb per instance.
+  val unique = HyperLogLogAggregator
+    .sizeAggregator(12)
+    //convert OwnerUserID to UTF-8 encoded bytes as HyperLogLog expects a byte array.
+    .composePrepare[StackExchange](_.OwnerUserID.toString.getBytes("UTF-8"))
 
-   val inaccuracy = 2D
+  val stackExchangePosts = TypedPipe.from(TypedTsv[StackExchange.StackExchangeType](input))
+    .map { StackExchange.fromTuple }
+    .aggregate(unique)
+    .map { x => println(s"Cardinality estimation (unique users in stack exchange data) : $x"); x }
+    .write(TypedTsv(output))
 
-   // Implicit conversion of text to bytes
-   implicit def long2Bytes(num:Long): Array[Byte] = num.toString.getBytes
-
-   // Helper method to print cardinality estimations on screen
-   def printSizeOfHLL(pipe: Pipe, symbol: Symbol, name:String ) =
-      pipe.mapTo( symbol -> symbol ) {
-        hll: DenseHLL =>
-          val estimation = hll.approximateSize.estimate
-          println(s"$name cardinality estimation: $estimation with $inaccuracy % error")
-          hll
-      }
-
-  val stackExchangePosts = Tsv(inputFiles,schema).read
-    .project('OwnerUserID)
-    .groupAll { group =>
-       group.hyperLogLog[Long]('OwnerUserID ->'denseHHL , inaccuracy)
-    }
-
-   printSizeOfHLL(stackExchangePosts, 'denseHHL, "stackexchange")
-     .write(TextLine(outputFile))
-
- }
+}
