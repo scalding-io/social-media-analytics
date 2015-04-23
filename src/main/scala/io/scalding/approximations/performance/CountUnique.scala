829a4e2fdd2208f2b833e23278c032b09818a467
Antonios Chalkiopoulos <Antwnis@gmail.com>
2015-04-23 11:40:15 +0100

    Testin performance of HLL

--- src/main/scala/io/scalding/approximations/performance/CountUnique.scala
+++ src/main/scala/io/scalding/approximations/performance/CountUnique.scala
@@ -5,8 +5,6 @@ import com.twitter.scalding._
 
 /**
  * A Scalding job to count unique elements using HLL
- *
- * Using a 3GB dataset - results in 23 Map tasks followed by 1 Reduce task
  */
 class CountUniqueHLL(args: Args) extends Job(args) {
 
@@ -31,15 +29,12 @@ class CountUniqueHLL(args: Args) extends Job(args) {
  */
 class CountUnique(args: Args) extends Job(args) {
 
-  val input = args.getOrElse("input", "datasets/100MillionUnique")
-  val output = args.getOrElse("output", "datasets/100MillionUniqueCardinality")
+  val input = args.getOrElse("input", "datasets/10MillionUnique")
+  val output = args.getOrElse("output", "datasets/10MillionUniqueCardinality")
 
   val countUniquePipe = TypedPipe.from(TextLine(input))
     .distinct
-    .groupAll
     .sum
-    //Throw away the unit key created by groupAll
-    .values
     .map { x => println(s"Cardinality of dataset: : $x"); x }
     .write(TypedTsv(output))
 
